ef80a773be16e192ddca9df72d1a7d906fca44d8
dong o kim <otwmoo@gmail.com>
2017-07-11 23:54:30 +0900

    react-firebase-blog-4 상세 정보 구체화 * 초기 상세

--- src/view/container/articles/ArticleDetail.jsx
+++ src/view/container/articles/ArticleDetail.jsx
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import articles from 'core/article/data';
 
 class ArticleDetail extends Component {
-  render(){
+  render() {
+    const
+       {
+        title,
+        content,
+        createDate,
+        creator,
+        modifiedDate,
+        modifier,
+    } = articles[0];
     return (
-      <article>
-        <p>
-          안녕하세요
-        </p>
-      </article>
+      <section>
+        <article>
+          <h4>{title}</h4>
+          <p>
+            {content}
+          </p>
+          <div>{createDate}</div>
+          <div>{creator}</div>
+          <div>{modifiedDate}</div>
+          <div>{modifier}</div>
+        </article>
+        <Link to="/">목록으로</Link>
+      </section>
     )
   }
 }
